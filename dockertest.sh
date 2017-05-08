6a03535bdf9146e03ed05b3c5b6f978089659b58
Samuel Schepp <samuel.schepp@mni.thm.de>
2017-05-08 23:55:43 +0200

    fixed test

--- dockertest.sh
+++ dockertest.sh
@@ -2,8 +2,8 @@ docker rm "swifty-ninja-test" -f
 # ignore exit code
 
 docker build -t "swifty-ninja" . && \
-docker run					\
--i							\
---name "swifty-ninja-test"	\
-"swifty-ninja"				\
-"swift test"
+docker run                 \
+-i                         \
+--name "swifty-ninja-test" \
+"swifty-ninja"             \
+swift test
